bca5ad2a0df184bb0fbdf6c6942776e2364a3dcf
Elvis888361 <elvisndegwa90@gmail.com>
2024-07-26 16:29:42 +0300

    commit2

--- aqiq_sales_and_purchase_report/service/item_changes.py
+++ aqiq_sales_and_purchase_report/service/item_changes.py
@@ -2,6 +2,5 @@ import frappe
 
 @frappe.whitelist()
 def item_before_insert(self):
-    this_doc=frappe.get_doc("Item",self)
-    company = frappe.get_doc("Company", this_doc.custom_company)
+    company = frappe.get_doc("Company", self)
     return company.custom_item_tax_default
